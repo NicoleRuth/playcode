29384b9c7fcf3d149fb6871a66f62cb74b6526b0
NicoleRuth <49164557+NicoleRuth@users.noreply.github.com>
2019-05-17 01:11:52 +0800

    Create index.java

---
+++ index.java
@@ -0,0 +1,78 @@
+<h1>Let's Start</h1>
+<p>
+1. The favorite clothing brand of your friend<br>
+<input type="radio" id="1A">A.Nike<br>
+<input type="radio" id="1B">B.Adidas<br>
+<input type="radio" id="1C">C.Bape<br>
+<input type="radio" id="1D">D.22fresh<br>
+<input type="radio" id="1E">E.Puma<br>
+<input type="radio" id="1F">F.Forever21<br>
+<input type="radio" id="1G">G.Zara<br>
+<input type="radio" id="1H">H.Champion<br>
+<input type="radio" id="1I">I.Roots<br>
+<input type="radio" id="1J">J.Herschel<br>
+</p>
+2. The favorite brand of chocolate bar of your friend<br>
+<input type="radio" id="1A">A.Coffee crisp<br>
+<input type="radio" id="1B">B.Kitkat<br>
+<input type="radio" id="1C">C.Snickers<br>
+<input type="radio" id="1D">D.Hershey's<br>
+<input type="radio" id="1E">E.Godiba<br>
+
+</p>
+3. The favorite ice cream flavor of your friend<br>
+<input type="radio" id="1A">A.Vanilla<br>
+<input type="radio" id="1B">B.Chocolate<br>
+<input type="radio" id="1C">C.Blueberry<br>
+<input type="radio" id="1D">D.Strawberry<br>
+<input type="radio" id="1E">E.Rum<br>
+<input type="radio" id="1F">F.Mocha<br>
+<input type="radio" id="1G">G.Marshmellow<br>
+</p>
+4. The favorite fast food brand of your friend<br>
+<input type="radio" id="1A">A.KFC<br>
+<input type="radio" id="1B">B.BurgerKing<br>
+<input type="radio" id="1C">C.Windy's<br>
+<input type="radio" id="1D">D.McDonald<br>
+<input type="radio" id="1E">E.DQ<br>
+</p>
+5. How do your friend usually drink coffee?<br>
+<input type="radio" id="1A">A.Add milk<br>
+<input type="radio" id="1B">B.Add sugar<br>
+<input type="radio" id="1C">C.Both milk and sugar<br>
+<input type="radio" id="1D">D.Add ice<br>
+<input type="radio" id="1E">E.Puma<br>
+</p>
+6. What does your friend usually do in his/her spare time?<br>
+<input type="radio" id="1A">A.Hang out with friends<br>
+<input type="radio" id="1B">B.Play video games<br>
+<input type="radio" id="1C">C.Be on the phone<br>
+<input type="radio" id="1D">D.Spend time with families<br>
+<input type="radio" id="1E">E.Do sports<br>
+<input type="radio" id="1F">F.Go shopping<br>
+</p>
+
+7. Which of the following colors suits your friend the most?<br>
+<input type="radio" id="1A">A.Red<br>
+<input type="radio" id="1B">B.Green<br>
+<input type="radio" id="1C">C.Blue<br>
+<input type="radio" id="1D">D.Orange<br>
+<input type="radio" id="1E">E.Purple<br>
+<input type="radio" id="1F">F.Yellow<br>
+<input type="radio" id="1G">G.Pink<br>
+<input type="radio" id="1H">H.Black<br>
+<input type="radio" id="1I">I.White<br>
+</p>
+8. The favorite dog's breed of your friend<br>
+<input type="radio" id="1A">A.Bulldog<br>
+<input type="radio" id="1B">B.Labrador retriver<br>
+<input type="radio" id="1C">C.Poodle<br>
+<input type="radio" id="1D">D.German shepherd<br>
+<input type="radio" id="1E">E.Beagle<br>
+<input type="radio" id="1F">F.Chihuahua<br>
+<input type="radio" id="1G">G.Gey hound<br>
+<input type="radio" id="1H">H.Golden retriever<br>
+<input type="radio" id="1I">I.None of above<br>
+</p>
+<button onclick="sub()">Submit</button>
+
